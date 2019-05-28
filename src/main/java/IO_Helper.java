6b50228d4bd5364762ee5d299289a75e18bab85c
Toffifee4me <philipp-schmidt2@gmx.net>
2019-05-28 11:34:11 +0200

    First

---
+++ src/main/java/IO_Helper.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class IO_Helper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null,"Hello");
+    }
+}
